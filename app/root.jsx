986ecbcc63c4670a5e23021bee9eefbf7aa49c6b
Talal Malick <talal.malick18@gmail.com>
2021-12-19 18:56:29 +0500

    initial commit

---
+++ app/root.jsx
@@ -0,0 +1,75 @@
+import { Fragment } from "react";
+import { Link, Links, Meta, LiveReload, Outlet } from "remix";
+import globalStylesUrl from "~/styles/global.css";
+
+export const links = () => [{ rel: "stylesheet", href: globalStylesUrl }];
+
+export const meta = () => {
+  const description = "A cool blog built with Remix";
+  const keywords = "remix, react, javascript";
+
+  return {
+    description,
+    keywords,
+  };
+};
+
+export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+function Document({ children, title }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Meta />
+        <Links />
+        <title>{title || "Remix Blog"}</title>
+      </head>
+      <body>
+        {children}
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <Fragment>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Remix
+        </Link>
+
+        <ul className="nav">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">{children}</div>
+    </Fragment>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  console.log(error);
+  return (
+    <Document>
+      <Layout>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+      </Layout>
+    </Document>
+  );
+}
